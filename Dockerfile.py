df4841bbe6e5081331c268ca0df86dedd3039a21
AL-Sayed2 <167108389+AL-Sayed2@users.noreply.github.com>
2025-04-08 10:23:18 +0300

    Update and rename Dockerfile to Dockerfile.py

--- Dockerfile.py
+++ Dockerfile.py
@@ -2,7 +2,7 @@ FROM python:3.10-slim
 
 # Install necessary dependencies
 RUN apt-get update && apt-get install -y \
-    libgl1-mesa-glx \
+    libglib2.0-0 \
     libsm6 \
     libxext6 \
     libxrender-dev \
